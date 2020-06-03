ece793b7bc63999e9bbe142c5e9137cd3649f94c
אסף שורץ <t5as@hotmail.com>
2020-06-03 07:57:20 +0300

    Added garagelogic classes

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Vehicle
+    {
+        private string m_ownerName;
+        private string m_ownerPhoneNumber;
+        private string m_carModel;
+        private string m_licenseNumber;
+        VehicleState m_vehicleState = VehicleState.InRepair;
+        private float m_energyPercentage;
+
+        enum VehicleState
+        {
+            InRepair,
+            Fixed,
+            Payed
+        }
+    }
+}
