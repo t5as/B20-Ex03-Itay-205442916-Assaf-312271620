add282f8eb4d81dc03d71027a3636568097e935e
Itay Neuberger <itayneu@gmail.com>
2020-06-16 22:07:02 +0300

    yo

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("yo");
+        }
+    }
+}
